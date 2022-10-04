92fac608b863d8b662ad0db632b1b1bc8be2f86d
Victoria Palma Rojas <palmarojas.victoria@gmail.com>
2022-10-03 21:50:41 -0300

    ahora se pueden agregar infinitos contactos nuevos

--- Contact_list.js
+++ Contact_list.js
@@ -2,14 +2,17 @@
 
 let contactos = [];
 
-function agregarContacto(contacto) {
-  contacto.id = prompt("Ingresar ID del contacto");
-  contacto.nombres = prompt("Ingresar nombres del contacto");
-  contacto.apellidos = prompt("Ingresar apellidos del contacto");
-  contacto.telefono = prompt("Ingresar número de teléfono del contacto");
-  contacto.ubicaciones = prompt("Ingresar ubicaciones del contacto");
-  contacto.ciudad = prompt("Ingresar ciudad del contacto");
-  contacto.direccion = prompt("Ingresar dirección del contacto");
+function agregarContacto() {
+  const contacto = {};
+  contacto.id = prompt("Ingresar ID del nuevo contacto");
+  contacto.nombres = prompt("Ingresar nombres del nuevo contacto");
+  contacto.apellidos = prompt("Ingresar apellidos del nuevo contacto");
+  contacto.telefono = prompt("Ingresar número de teléfono del nuevo contacto");
+  contacto.ubicaciones = {};
+  contacto.ubicaciones.ciudad = prompt("Ingresar ciudad del nuevo contacto");
+  contacto.ubicaciones.direccion = prompt(
+    "Ingresar dirección del nuevo contacto"
+  );
   contactos.push(contacto);
 }
 
@@ -17,8 +20,8 @@ function eliminarContacto(contacto) {
   contactos = contactos.filter((nombre) => contacto !== nombre);
 }
 
-agregarContacto(contacto);
-imprimirContactos;
+agregarContacto();
+imprimirContactos();
 
 contacto = prompt("Ingresar nombre del contacto a eliminar: ");
 eliminarContacto(contacto);
